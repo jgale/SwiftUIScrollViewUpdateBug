21150868ac353948712e61ec3ff2278a40f05524
Jeremy Gale <jeremy.gale@atco.com>
2020-07-21 17:14:42 -0600

    Initial Commit

---
+++ ScrollViewUpdateBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollViewUpdateBug
+//
+//  Created by Jeremy Gale on 2020-07-21.
+//  Copyright © 2020 Jeremy Gale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
