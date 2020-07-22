a4b202fa894c380481fe871fa8ff6b1c93c07d4b
Jeremy Gale <jeremy.gale@atco.com>
2020-07-22 08:36:56 -0600

    Add the minimal reproduction case

--- ScrollViewUpdateBug/ContentView.swift
+++ ScrollViewUpdateBug/ContentView.swift
@@ -8,9 +8,32 @@
 
 import SwiftUI
 
+struct Object: Identifiable {
+    var id: String
+}
+
 struct ContentView: View {
+    @State var array = [Object]()
+
     var body: some View {
-        Text("Hello, World!")
+        //VStack { // uncomment the VStack to see that it works perfectly fine
+            ScrollView() {
+                // Uncomment this rectangle to see it work perfectly fine
+                //Rectangle().frame(width: UIScreen.main.bounds.size.width, height: 0.01)
+                ForEach(array) { object in
+                    Text(object.id)
+                }
+            }
+        //} // uncomment the VStack to see that it works perfectly fine
+            .onAppear(perform: {
+                self.array = [
+                    Object(id: "1"),
+                    Object(id: "2"),
+                    Object(id: "3"),
+                    Object(id: "4"),
+                    Object(id: "5")
+                ]
+            })
     }
 }
 
